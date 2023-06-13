47e17b989d90a81f8788077ad64528ad303d7782
José Clévison <cdefensor467@gmail.com>
2023-06-13 11:13:15 -0300

    quase  finalizado

--- Meu-Portfolio/app.js
+++ Meu-Portfolio/app.js
@@ -6,7 +6,7 @@ window.addEventListener('scroll', function() {
 
 
 
-
+//meu portfolio projects
 
 document.addEventListener("DOMContentLoaded", function() {
   const portfolioItems = document.querySelectorAll(".portfolio-item");
@@ -48,4 +48,60 @@ document.addEventListener("DOMContentLoaded", function() {
 
     showImage(currentImageIndex);
   });
+});
+
+//meu portfolio projects pessoais
+
+document.addEventListener('DOMContentLoaded', function() {
+  const portfolio = document.querySelector('.meu-portfolio');
+  const prevButton = document.querySelector('.prev');
+  const nextButton = document.querySelector('.next');
+  const images = Array.from(portfolio.querySelectorAll('.img-pessoal'));
+
+  const visibleImages = 4;
+  const hiddenImages = images.length - visibleImages;
+  let currentOffset = 0;
+
+  function updateButtonVisibility() {
+    prevButton.style.display = currentOffset === 0 ? 'none' : 'block';
+    nextButton.style.display = currentOffset >= hiddenImages ? 'none' : 'block';
+  }
+
+  function updatePortfolioOffset() {
+    portfolio.style.transform = `translateX(-${currentOffset * 320}px)`;
+  }
+
+  function showVisibleImages() {
+    images.forEach(function(image, index) {
+      if (index >= currentOffset && index < currentOffset + visibleImages) {
+        image.style.display = 'block';
+      } else {
+        image.style.display = 'none';
+      }
+    });
+  }
+
+  function handlePrevButtonClick() {
+    if (currentOffset > 0) {
+      currentOffset--;
+      updatePortfolioOffset();
+      updateButtonVisibility();
+      showVisibleImages();
+    }
+  }
+
+  function handleNextButtonClick() {
+    if (currentOffset < hiddenImages) {
+      currentOffset++;
+      updatePortfolioOffset();
+      updateButtonVisibility();
+      showVisibleImages();
+    }
+  }
+
+  prevButton.addEventListener('click', handlePrevButtonClick);
+  nextButton.addEventListener('click', handleNextButtonClick);
+
+  showVisibleImages();
+  updateButtonVisibility();
 });
