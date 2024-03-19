1eb1b912b7e682e9e9ffa803939daf67c8717b37
José Clévison <cdefensor467@gmail.com>
2024-03-19 01:26:03 -0300

    change the personal projects

--- Meu-Portfolio/script.js
+++ Meu-Portfolio/script.js
@@ -23,90 +23,17 @@ btn.addEventListener('click', () => {
 // =======================================
 
 // Mobile responsividade
-const prevButton = document.getElementById('prev');
-const nextButton = document.getElementById('next');
-const content = document.getElementById('content');
-const items = document.getElementsByClassName('portfolio-item');
+window.addEventListener('DOMContentLoaded', () => {
+  const portfolio = document.querySelector('.meu-portfolio');
+  const images = portfolio.querySelectorAll('.img-pessoal');
+  const gapSize = 2 * parseInt(window.getComputedStyle(portfolio).gap);
 
-let currentItem = 0;
-const totalItems = items.length;
-let itemsPerView = 4;
-const autoInterval = 3000;
-let autoMode = true;
+  // Calcula a largura total do portfolio com base no número de imagens e no espaçamento entre elas
+  const totalWidth = (images.length * images[0].offsetWidth) + ((images.length - 1) * gapSize);
 
-const showCurrentItems = () => {
-  for (let i = 0; i < totalItems; i++) {
-    items[i].style.display = 'none';
-  }
-
-  for (let i = currentItem; i < currentItem + itemsPerView; i++) {
-    const index = i % totalItems;
-    items[index].style.display = 'block';
-  }
-};
-
-const nextItem = () => {
-  currentItem++;
-  if (currentItem >= totalItems) {
-    currentItem = 0;
-  }
-  showCurrentItems();
-};
-
-const checkWindowSize = () => {
-  const windowWidth = window.innerWidth;
-  if (windowWidth <= 600) {
-    itemsPerView = 1;
-  } else if (windowWidth >= 601 && windowWidth <= 768) {
-    itemsPerView = 2;
-  } else if (windowWidth >= 769 && windowWidth <= 992) {
-    itemsPerView = 3;
-  } else if (windowWidth >= 993 && windowWidth <= 1199) {
-    itemsPerView = 3;
-  }  else {
-    itemsPerView = 4;
-  }
-};
-
-prevButton.addEventListener('click', () => {
-  autoMode = false;
-  currentItem--;
-  if (currentItem < 0) {
-    currentItem = totalItems - 1;
-  }
-  showCurrentItems();
+  // Define a largura do portfolio
+  portfolio.style.width = totalWidth + 'px';
 });
-
-nextButton.addEventListener('click', () => {
-  autoMode = false;
-  nextItem();
-});
-
-for (let i = 0; i < totalItems; i++) {
-  items[i].addEventListener('mouseover', () => {
-    autoMode = false;
-  });
-
-  items[i].addEventListener('mouseout', () => {
-    autoMode = true;
-  });
-}
-
-window.addEventListener('resize', () => {
-  checkWindowSize();
-  showCurrentItems();
-});
-
-checkWindowSize();
-showCurrentItems();
-items[currentItem].classList.add('active');
-
-setInterval(() => {
-  if (autoMode) {
-    nextItem();
-  }
-}, autoInterval);
-
 
 
 //==========================================================
@@ -114,7 +41,7 @@ setInterval(() => {
 document.addEventListener('DOMContentLoaded', function() {
   const prevButton = document.querySelector('.prev3');
   const nextButton = document.querySelector('.next3');
-  const portfolio = document.querySelector('.tra-portfolio');
+  const portfolio = document.querySelector('.portfolio-trabalho'); // Corrigido o seletor aqui
   const images = document.querySelectorAll('.img-tra');
 
   let currentIndex = 0;
